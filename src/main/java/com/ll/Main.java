25c76dd082cb1f4857e8d1fb256fb50b407588ea
jhs512 <jangka512@gmail.com>
2024-03-02 17:16:49 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,6 +9,14 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("계산기, 시작");
+
+        System.out.print("입력 : ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력한 값: " + cmd);
 
         System.out.println("계산기, 끝");
     }
